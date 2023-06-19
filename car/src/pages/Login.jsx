ad83f380357d5e357ec2fa48c62394015086f72e
Sema <semaesgerova68@gmail.com>
2023-06-19 22:17:25 +0400

    first commit

---
+++ car/src/pages/Login.jsx
@@ -0,0 +1,41 @@
+import React from 'react'
+import { Col, Row, Button } from 'react-bootstrap'
+import { LinkContainer } from 'react-router-bootstrap'
+import { NavLink } from 'react-router-dom'
+
+const Login = () => {
+  return (
+    <div className='login'>
+        <Row className='m-0'>
+            <Col sm={12} md={5} className='p-0'>
+                <div className="img"></div>
+            </Col>
+            <Col sm={12} md={7}>
+                <div className="text d-flex flex-column justify-content-center align-items-center">
+                    <div className="top text-center">
+                      <h1>YellowGo</h1>
+                      <h3>Your Rental, Your Journey</h3>
+                      <p>Welcome, please log in to your account.</p>
+                    </div>
+                    <div className="bottom d-flex flex-column justify-content-center align-items-center">
+                     <div className="inputs d-flex flex-column">
+                     <input type="email" placeholder='Email address'/>
+                      <input type="password" placeholder='Password'/>
+                      <p className='text-end'>Forgot password?</p>
+                     </div>
+                      <Button>Log in</Button>
+                      <p>or log in with</p>
+                      <div className="box d-flex justify-content-center align-items-center">
+                      <Button>Google</Button>
+                      <Button>Facebook</Button>
+                      </div>
+                      <p>Dont have an account? <LinkContainer to='/'><NavLink>Sign Up</NavLink></LinkContainer></p>
+                    </div>
+                </div>
+            </Col>
+        </Row>
+    </div>
+  )
+}
+
+export default Login
