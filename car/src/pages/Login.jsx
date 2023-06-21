738cb21466d9fcb4aa5475cfc9fce236e30b63a0
Sema <semaesgerova68@gmail.com>
2023-06-21 10:35:09 +0400

    form validation

--- car/src/pages/Login.jsx
+++ car/src/pages/Login.jsx
@@ -1,9 +1,28 @@
 import React from 'react'
-import { Col, Row, Button } from 'react-bootstrap'
+import { Col, Row, Button, Form } from 'react-bootstrap'
 import { LinkContainer } from 'react-router-bootstrap'
 import { NavLink } from 'react-router-dom'
+import { useForm } from 'react-hook-form';
+import * as yup from 'yup';
+import {yupResolver} from '@hookform/resolvers/yup'
+
+
 
 const Login = () => {
+
+  const schema = yup.object().shape({
+    email: yup.string().email().required(),
+    password: yup.string().min(8).max(20).required()
+})
+
+const {register, handleSubmit, formState: {errors}} = useForm({
+  resolver: yupResolver(schema),
+});
+
+  const onSubmit = (data) =>{
+    console.log(data);
+  }
+
   return (
     <div className='login'>
         <Row className='m-0'>
@@ -14,21 +33,22 @@ const Login = () => {
                 <div className="text d-flex flex-column justify-content-center align-items-center">
                     <div className="top text-center">
                       <h1>YellowGo</h1>
-                      <h3>Your Rental, Your Journey</h3>
-                      <p>Welcome, please log in to your account.</p>
+                      <div className="topText2 d-flex flex-column">
+                        <h3>Your Rental, Your Journey</h3>
+                        <p>Welcome, please log in to your account.</p>
+                      </div>
                     </div>
                     <div className="bottom d-flex flex-column justify-content-center align-items-center">
+                     <Form onSubmit={handleSubmit(onSubmit)}>
                      <div className="inputs d-flex flex-column">
-                     <input type="email" placeholder='Email address'/>
-                      <input type="password" placeholder='Password'/>
+                     <input type="email" placeholder='Email address' {...register('email')}/>
+                     {errors.email && <p className='txt'>{errors.email?.message}</p>}
+                      <input type="password" placeholder='Password' {...register('password')}/>
+                      {errors.password && <p className='txt'>{errors.password?.message}</p>}
                       <p className='text-end'>Forgot password?</p>
                      </div>
-                      <Button>Log in</Button>
-                      <p>or log in with</p>
-                      <div className="box d-flex justify-content-center align-items-center">
-                      <Button>Google</Button>
-                      <Button>Facebook</Button>
-                      </div>
+                      <Button type='submit'>Log in</Button>
+                     </Form>
                       <p>Dont have an account? <LinkContainer to='/'><NavLink>Sign Up</NavLink></LinkContainer></p>
                     </div>
                 </div>
--- car/src/pages/SignUp.jsx
+++ car/src/pages/SignUp.jsx
@@ -1,9 +1,27 @@
 import React from 'react'
-import { Col, Row, Button } from 'react-bootstrap'
+import { Col, Row, Button, Form } from 'react-bootstrap'
 import { LinkContainer } from 'react-router-bootstrap'
 import { NavLink } from 'react-router-dom'
+import { useForm } from 'react-hook-form';
+import * as yup from 'yup';
+import {yupResolver} from '@hookform/resolvers/yup'
 
 const SignUp = () => {
+  
+  const schema = yup.object().shape({
+    email: yup.string().email().required(),
+    password: yup.string().min(8).max(20).required(),
+    confirmPassword: yup.string().oneOf([yup.ref("password"),null], "Passwords Don't Match").required()
+})
+
+const {register, handleSubmit, formState: {errors}} = useForm({
+  resolver: yupResolver(schema),
+});
+
+  const onSubmit = (data) =>{
+    console.log(data);
+  }
+  
   return (
     <div className='signUp'>
         <Row className='m-0'>
@@ -14,21 +32,23 @@ const SignUp = () => {
                 <div className="text d-flex flex-column justify-content-center align-items-center">
                     <div className="top text-center">
                       <h1>YellowGo</h1>
-                      <h3>Start Your Journey Here</h3>
-                      <p>Experience the Freedom of Renting</p>
+                      <div className="topText2">
+                         <h3>Start Your Journey Here</h3>
+                         <p>Experience the Freedom of Renting</p>
+                      </div>
                     </div>
                     <div className="bottom d-flex flex-column justify-content-center align-items-center">
-                     <div className="inputs d-flex flex-column">
-                     <input type="email" placeholder='Email address'/>
-                      <input type="password" placeholder='Password'/>
-                      <input type="password" placeholder='Re-type Password'/>
+                    <Form onSubmit={handleSubmit(onSubmit)}>
+                    <div className="inputs d-flex flex-column">
+                     <input type="email" placeholder='Email address' {...register('email')}/>
+                     {errors.email && <p>{errors.email?.message}</p>}
+                      <input type="password" placeholder='Password' {...register('password')}/>
+                      {errors.password && <p>{errors.password?.message}</p>}
+                      <input type="password" placeholder='Re-type Password' {...register('confirmPassword')}/>
+                      {errors.confirmPassword && <p>{errors.confirmPassword?.message}</p>}
                      </div>
-                      <Button>Sign Up</Button>
-                      <p>or sign up with</p>
-                      <div className="box d-flex justify-content-center align-items-center">
-                      <Button>Google</Button>
-                      <Button>Facebook</Button>
-                      </div>
+                      <Button type='submit'>Sign Up</Button>
+                    </Form>
                       <p>Already have an account? <LinkContainer to='/login'><NavLink>Log in</NavLink></LinkContainer></p>
                     </div>
                 </div>
